1b40ff121c9f8e3641e44cfdb69a5a36f27337d4
jedan <ahmadzaydan921@gmail.com>
2026-06-02 23:29:25 +0700

    Update categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -1,15 +1,12 @@
-// routes/categories.js
+
 const express = require('express');
 const router = express.Router();
 
-// GET /api/categories
 router.get('/', async (req, res) => {
     const pool = req.app.get('db_pool'); // Mengambil pool dari index.js
     try {
         console.log('📬 Request kategori diterima oleh Backend...');
-        
-        // PENTING: Pastikan nama tabel Anda adalah 'categories'
-        // Jika nama kolom Anda berbeda (misal: id_category, name), sesuaikan di bawah ini
+    
         const result = await pool.query('SELECT name FROM categories ORDER BY name ASC');
         
         console.log(`🟢 Berhasil mengambil ${result.rows.length} kategori dari database`);
